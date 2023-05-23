d4c8bd55ddd1cd0e211d2f3eaff6aeb631464560
fuad-07 <fmfuad07@gmail.com>
2023-05-23 12:30:38 +0600

    day 2 completed

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:demo_learning/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     home: HomePage(),
+    );
+  }
+}
